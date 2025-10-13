fc1cc8f4a84070590470514549f5cfa8c25cef9e
unknown <onyinyechidikeocha3@gmail.com>
2025-10-13 20:47:42 +0100

    optimization changes

--- src/app/(pages)/sports/basketball/basketballPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const BasketballPage = () => {
-  return (
-    <div className='text-xl text-red-300' >BasketballPage</div>
-  )
-}
-
-export default BasketballPage
--- src/app/(pages)/sports/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>this is the sports page</div>
-  )
-}
-
-export default page
